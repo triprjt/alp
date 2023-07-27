bad0bf1d5835b3e6e9a18ca2f29f817b8be58e26
triprjt <triprjt@gmail.com>
2023-07-27 11:09:15 +0530

    .

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
-# echo " BUILD START"
+echo " BUILD START"
 # python -m pip install -r requirements.txt
 # python manage.py collectstatic --noinput --clear
-# echo " BUILD END" 
 pip install -r requirements.txt
 python3.9 manage.py collectstatic
+echo " BUILD END" 
